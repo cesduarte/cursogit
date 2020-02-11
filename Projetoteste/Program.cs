9348feadcfefc68aaa89c9a10fafb748ef9a8860
Carlos <carlos.e.s.duarte@hotmail.com>
2020-02-11 00:05:54 -0300

    foi criado uma mensagem de bom dia

--- Projetoteste/Program.cs
+++ Projetoteste/Program.cs
@@ -7,6 +7,7 @@ namespace Projetoteste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
